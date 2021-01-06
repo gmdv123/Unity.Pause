5785a36a5f6b0ffae325ae31112fc2484d32202c
su1904 <57542677+gmdv123@users.noreply.github.com>
2021-01-06 19:40:31 +0700

    Simple pause function

---
+++ PauseMenu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePause = false;
+    public GameObject PauseMenuUI;
+    public Button yourButton;
+    private void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePause)
+            {
+                
+                ResumeF();
+            }
+            else
+            {
+                
+                PauseF();
+            }
+        }
+
+    }
+    void TaskOnClick()
+    {
+        if (GamePause)
+        {
+            ResumeF();
+        }
+        else
+        {
+            PauseF();
+        }
+    }
+    public void ResumeF()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePause = false;
+        FindObjectOfType<AudioManager>().Play("Car");
+    }
+    void PauseF()
+    {
+        FindObjectOfType<AudioManager>().StopPlaying("Car");
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePause = true;
+    }
+    public void MainMenu()
+    {
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("StartMenu");
+    }
+    public void Retry()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Gameplay");
+    }
+}
